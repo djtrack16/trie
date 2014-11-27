2108d23a76fdb03fce743b09089ac08b5cd06ee3
Darius Liddell <djtrack16@gmail.com>
2014-11-27 00:13:44 -0800

    perf details

--- test_trie.py
+++ test_trie.py
@@ -1,5 +1,6 @@
 from trie import Trie
 import random
+import time
 
 test_prefixes = {
 	'abo': set([]),
@@ -15,27 +16,44 @@ test_prefixes = {
 }
 def test_add_and_contains(words, prefixLen):
 	t = Trie()
-	#test_prefixes = {} # as we build the tree, we get all the words starting with the prefixes we want to test
+	maxInsert = 0
+	maxContains = 0
 	for w in words:
+
+		add_start = time.time()
 		t.addWord(w)
+		add_end = time.time() - add_start
+		maxInsert = max(maxInsert, add_end)
+
 		if len(w) >= prefixLen:
 			if w[:prefixLen] in test_prefixes:
 				test_prefixes[w[:prefixLen]].add(w)
+
+		contains_start = time.time()
 		assert(w in t)
+		contains_end = time.time() - contains_start
+		maxContains = max(maxContains, contains_end)
+
+	print 'Max time to add word was %f sec, Max time to check if word in trie was %f sec' % (maxInsert, maxContains)
+
 	return t
 
 def test_autocomplete(t, words):
 	# how do we know the words that the algorithm finds are all the possible words
 	for prefix in test_prefixes.keys():
-		#print t.autocomplete(prefix).symmetric_difference(test_prefixes[prefix])
-		assert(t.autocomplete(prefix) == test_prefixes[prefix])
+		# we can check the equality of sets this way or ask if symmetric difference is 0
+		start = time.time()
+		completions = t.autocomplete(prefix)
+		assert(completions == test_prefixes[prefix])
+		print 'Autocomplete time for prefix "%s" with %d words was %f sec' % (prefix, len(completions), time.time() - start) 
+
+
 
 ''' Generate an arbitrary prefix of length prefixLen and add it to dictionary '''
 def getPrefixes(prefixLen):
 	pass
 
 if __name__ == '__main__':
-	#words = ['baby', 'food', 'peach', 'peaches', 'pie', 'doll', 'wisdom', 'rosemary', 'dolt', 'sun', 'sundry']
 	t = Trie()
 	'''
 	Read in a large text file from path using 'cat /usr/share/dict/words > words.txt' (235K words) 
