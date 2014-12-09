63147c078da825d7d8fbd6d431ecb26fcc8ddad2
Darius Liddell <djtrack16@gmail.com>
2014-12-08 17:43:53 -0800

    delete words testing

--- test_trie.py
+++ test_trie.py
@@ -14,6 +14,7 @@ test_prefixes = {
 	'que': set([]),
 	'coc': set([]),
 }
+
 def test_add_and_contains(words, prefixLen):
 	t = Trie()
 	maxInsert = 0
@@ -21,7 +22,7 @@ def test_add_and_contains(words, prefixLen):
 	for w in words:
 
 		add_start = time.time()
-		t.addWord(w)
+		t.add(w)
 		add_end = time.time() - add_start
 		maxInsert = max(maxInsert, add_end)
 
@@ -47,21 +48,42 @@ def test_autocomplete(t, words):
 		assert(completions == test_prefixes[prefix])
 		print 'Autocomplete time for prefix "%s" with %d words was %f sec' % (prefix, len(completions), time.time() - start) 
 
+'''
+Pops arbitrary elements from dictionary and tries to delete them, afterwards exhaustively checking that thiss didn't screw up any other word
+in trie. Adds words back to dictionary and trie for next round of testing. Does this 'trials' number of times
+'''
+def test_batch_delete(t, words, trials, toDelete):
+	for trial in xrange(trials):
+		prefixes = set([])
+		for x in xrange(toDelete):
+			elem = words.pop()
+			prefixes.add(elem)
+			t.delete(elem)
+		for w in words:
+			# the only elements not in trie are the ones we deleted
+			if not w in t:
+				assert(w in prefixes)
+		for pre in prefixes:
+			words.add(pre)
+			t.add(pre)
 
 
-''' Generate an arbitrary prefix of length prefixLen and add it to dictionary '''
-def getPrefixes(prefixLen):
-	pass
-
-if __name__ == '__main__':
-	t = Trie()
-	'''
-	Read in a large text file from path using 'cat /usr/share/dict/words > words.txt' (235K words) 
-	'''
+'''
+Read in a large text file from path using 'cat /usr/share/dict/words > words.txt' (235K words) 
+'''
+def dictionary():
 	f = open('./words.txt')
 	words = set([])
+	ct = 0
 	for line in f:
 		words.add(line[:-1])
+		ct += 1
+		if ct > 10000:
+			break
+	return words
 
-	test_add_and_contains(words, 3)
-	test_autocomplete(t, words)
+if __name__ == '__main__':
+	words = dictionary()
+	t = test_add_and_contains(words, 3)
+	#test_autocomplete(t, words)
+	test_batch_delete(t, words, 1000, 100)
