970d250d1bba1814d6a9b16cd8bd07e6ea4221a7
Darius Liddell <djtrack16@gmail.com>
2015-02-12 22:01:10 -0800

    refactor

--- trie.py
+++ trie.py
@@ -1,32 +1,39 @@
 class Trie():
-
-	''' Naive trie implementation
-
-		Complexity:
-		Time: O(length of word)
-		Space: O(M^n) where M = alphabet size (in our case, 26) and N = height of tree
-		Every node is a dictionary, listing all the possible suffixes of this character so far in the trie
 	'''
-
+	Naive trie implementation
+	Complexity:
+	Time: O(length of word)
+	Space: O(M^n) where M = alphabet size (in our case, 26) and N = height of tree
+	'''
 	def __init__(self, children={}, endOfWord=False):
 		self.children = children
 		self.endOfWord = endOfWord
 
 	def add(self, word):
+		"""
+		Adds a word to the trie.
+		If word already in trie, changes nothing.
+		If given empty string -> does nothing
+		"""
 		next = self
 		for char in word:
 			if not char in next.children:
 				next.children[char] = Trie({})
 			next = next.children[char]
-		next.endOfWord = True
+		if word:
+			next.endOfWord = True
 
 	def delete(self, word):
-		# Four cases:
-		# 1. Word is not in trie: do nothing
-		# 2. Word does not contain prefix of another word or is the prefix of any other word: delete all nodes in word
-		# 3. Word is prefix of another word in trie: set node.endOfWord to False, and we are done
-		# 4. Another word is prefix of this word in trie: delete leaf nodes bottom-up until the most recent endofword flag
-
+		"""
+		Delete word from trie.
+		Four cases:
+		1. Word is not in trie -> do nothing
+		2. Word is in trie, but does not contain prefix or suffix of other words in tree,
+		   -> delete entire word
+		3. Word is prefix of another word in trie: set node.endOfWord to False, and we are done
+		4. Another word is prefix of this word in trie: delete leaf nodes bottom-up until the
+		   most recent endofword flag
+		"""
 		node = self
 		# base case
 		if node.endOfWord and not word:
@@ -53,21 +60,24 @@ class Trie():
 
 
 
-	def __contains__(self, word): 
+	def __contains__(self, word):
+		'''
+		Returns true if words is in trie.
+		Otherwise, returns false.
+		Note: Only if endOfWord is marked on last letter in word, is the word in the trie.
+		''' 
 		node = self
 		for char in word:
 			if not char in node.children:
 				return False
 			node = node.children[char]
-		# only a word if we have found all characters in the word in correct order
-		# and our stopping place happens to have 'endofword == True'
 		return node.endOfWord
 	
-	''' 
-	Find the node in the tree where we should begin our autocomplete.
-	Then return all suffixes ending with this word
-	'''
 	def autocomplete(self, prefix):
+		''' 
+		Find the ending of the prefix in the tree where we should begin our autocomplete.
+		Then returns a *generator* of all words beginning with this prefix
+		'''
 		node = self
 		for char in prefix:
 			if not char in node.children:
@@ -75,19 +85,19 @@ class Trie():
 				#print 'Prefix \'%s\' doesn\'t exist in trie' % (prefix)
 				return []
 			node = node.children[char]
-		words = set([])
-		node.allSuffixes(prefix, words)
-		return words
+		return node.allSuffixes(prefix)
 
-	'''
-	Generate all possible suffixes of a given prefix
-	E.g. If given the empty string, will print all words in trie
-	If given a prefix that is known to exist in tree, will give all words starting with that prefix (autocomplete feature)
-	'''
-	def allSuffixes(self, suffix, words):
-		prefix = self.children
+	def allSuffixes(self, prefix):
+		'''
+		Generate all possible words of a given prefix
+		E.g. If given the empty string, will print all words in trie
+		if given a prefix that is known to exist in tree, will give all words starting with that prefix (autocomplete feature)
+		'''
+		remaining_letters = self.children
 		if self.endOfWord:
-			words.add(suffix)
+			yield prefix
 
-		for char in prefix.keys():
-			prefix[char].allSuffixes(suffix+char, words)
+		for char in remaining_letters.keys():
+			prefix = ''.join([prefix, char])
+			for word in remaining_letters[char].allSuffixes(prefix):
+				yield word
